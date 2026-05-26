cc5fa5f3cc0060f21971fe243d9a70e7f79e6e12
anushamanglik <84149024+anushamanglik@users.noreply.github.com>
2026-05-26 16:57:43 -0400

    fix

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router";
 import Home from "./pages/Home";
 import CocktailRecipe from "./pages/CocktailRecipe";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "/cocktail/:id",
-    Component: CocktailRecipe,
-  },
-]);
+export const router = createBrowserRouter([ 
+  { 
+    path: "/", 
+    Component: Home, 
+  }, 
+  { 
+    path: "/cocktail/:id", 
+    Component: CocktailRecipe, 
+  }, 
+], {
+  // Add this configuration object
+  basename: "/Thankyoubill", 
+});
